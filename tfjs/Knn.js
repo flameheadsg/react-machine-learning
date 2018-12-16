0aa511dcd08123508c6d07833c9ce9833c15c6f4
Sunny Gupta <flameheadsg@gmail.com>
2018-12-15 17:37:54 -0800

    fixed KNN class

--- tfjs/Knn.js
+++ tfjs/Knn.js
@@ -16,10 +16,9 @@ class Knn {
 
   predict(beds, baths, floors, sqft, k) {
     let testPoint = tf.tensor([ beds, baths, sqft, floors]);
-    let { price, closest } = this.knn(this.trainFeatures, this.trainLabels, testPoint, k, true);
+    let price = this.knn(this.trainFeatures, this.trainLabels, testPoint, k);
     return {
       price,
-      closest,
       resBeds: beds,
       resBaths: baths,
       resFloors: floors,
@@ -48,45 +47,23 @@ class Knn {
     return { mean: avg, stddev, max };
   }
 
-  knn(features, labels, predictionPoint, k, prediction = false) {
+  knn(features, labels, predictionPoint, k) {
     const { mean, variance } = tf.moments(features, 0);
     const scaledPrediction = predictionPoint.sub(mean).div(variance.pow(0.5));
 
-    if (prediction) {
-      let sorted = features
-        .sub(mean)
-        .div(variance.pow(0.5))
-        .sub(scaledPrediction)
-        .pow(2)
-        .sum(1)
-        .pow(0.5)
-        .expandDims(1)
-        .concat(labels, 1)
-        .unstack()
-        .sort((a, b) => a.get(0) > b.get(0) ? 1 : -1);
-
-      let price = sorted
-        .slice(0, k)
-        .reduce((acc, pair) => acc + pair.get(1), 0) / k;
-
-      let closest = sorted[0].get(1);
-
-      return { price, closest };
-    } else {
-      return features
-        .sub(mean)
-        .div(variance.pow(0.5))
-        .sub(scaledPrediction)
-        .pow(2)
-        .sum(1)
-        .pow(0.5)
-        .expandDims(1)
-        .concat(labels, 1)
-        .unstack()
-        .sort((a, b) => a.get(0) > b.get(0) ? 1 : -1)
-        .slice(0, k)
-        .reduce((acc, pair) => acc + pair.get(1), 0) / k;
-    }
+    return features
+      .sub(mean)
+      .div(variance.pow(0.5))
+      .sub(scaledPrediction)
+      .pow(2)
+      .sum(1)
+      .pow(0.5)
+      .expandDims(1)
+      .concat(labels, 1)
+      .unstack()
+      .sort((a, b) => a.get(0) > b.get(0) ? 1 : -1)
+      .slice(0, k)
+      .reduce((acc, pair) => acc + pair.get(1), 0) / k;
   }
 }
 
